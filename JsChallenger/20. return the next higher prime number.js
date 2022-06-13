70bd7a19c03b3ac9ddf4c6aa0ca4575505d1c676
A Iman <akhmadimannug@gmail.com>
2022-06-13 20:39:29 +0700

    add new challenge

--- JsChallenger/20. return the next higher prime number.js
+++ JsChallenger/20. return the next higher prime number.js
@@ -17,6 +17,20 @@ function myFunction(num) {
   return num - 1;
 }
 
+/* 
+function myFunction( a ) {
+  function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return num > 1;
+  }
+ let n = a;
+ while (!isPrime(n)) n++;
+return n
+}
+*/
+
 console.log(myFunction(38)); // 41
 console.log(myFunction(7)); // 7
 console.log(myFunction(115)); // 127
